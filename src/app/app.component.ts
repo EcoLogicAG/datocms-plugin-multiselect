3876374e67fc27a33d7e2b8db4361aa57bbfa9ee
Markus Huggler <markus.huggler@ecologic.ch>
2018-11-05 23:24:55 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import DatoCmsPlugin from 'datocms-plugins-sdk';
+
+interface IOption {
+    key: string;
+    value: string;
+    selected: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'datocms-plugin-multiselect';
+export class AppComponent implements OnInit {
+
+    public fieldName: string;
+    public options: IOption[] = [];
+    public currentSelection: string[] = [];
+    public plugin: any;
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        DatoCmsPlugin.init((plg) => {
+            this.plugin = plg;
+            this.plugin.startAutoResizer();
+
+            this.fieldName = this.plugin.field.attributes.label;
+
+            this.currentSelection = JSON.parse(this.plugin.getFieldValue(this.plugin.fieldPath)) || [];
+
+            const parameters = JSON.parse(this.plugin.parameters.instance.options);
+            parameters.forEach((option) => {
+                this.options.push({
+                    key: option.key,
+                    value: option.value,
+                    selected: this.currentSelection.indexOf(option.key) >= 0
+                });
+            });
+        });
+    }
+
+    public onCheckChange(option: IOption) {
+        option.selected = !option.selected;
+        if (option.selected) {
+            this.currentSelection.push(option.key);
+        } else {
+            this.currentSelection.splice(this.currentSelection.indexOf(option.key), 1);
+        }
+        this.plugin.setFieldValue(this.plugin.fieldPath, JSON.stringify(this.currentSelection));
+    }
+
 }
